53731dd8939408668c04b40efc5a74b234e48c0d
PetroGal <petergalp@gmail.com>
2022-01-17 10:20:59 +0200

    refactor with key prop and spread operator

--- src/index.js
+++ src/index.js
@@ -5,49 +5,46 @@ import './index.css'
 
 //setup vars
 // let's refactor these three values into an object
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY436_FMwebp_QL65_.jpg',
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth',
-}
 
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY436_FMwebp_QL65_.jpg',
-  title: 'Our Class is a Family',
-  author: 'Shannon Olsen ',
-}
+const books = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/41w4B0f21VL._SY349_BO1,204,203,200_.jpg',
+    title: 'The Very Hungry Caterpillar',
+    author: 'Eric Carle',
+  },
+]
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae
-          quibusdam assumenda beatae consequatur temporibus rem iusto dolores
-          dolor illo natus?
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book, index) => {
+        const { img, title, author } = book
+        return <Book key={book.id} {...book}></Book>
+      })}
     </section>
   )
 }
 
-const Book = (props) => {
-  const { img, title, author, children } = props
+const Book = ({ img, title, author }) => {
+  //const { img, title, author } = props
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
